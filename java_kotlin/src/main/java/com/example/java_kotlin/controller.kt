966b6cd629250ab0ff293409675aaaf7682d0c2b
Bock <bockyanggoh@gmail.com>
2021-05-21 11:34:28 +0800

    test 123

--- java_kotlin/src/main/java/com/example/java_kotlin/controller.kt
+++ java_kotlin/src/main/java/com/example/java_kotlin/controller.kt
@@ -1,9 +1,19 @@
 package com.example.java_kotlin
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api")
@@ -12,4 +22,15 @@ class SimpleController {
     fun getApi(): ResponseEntity<String> {
         return ResponseEntity.ok("Success!")
     }
+
+    @PostMapping
+    fun postApi(@RequestBody x: SimpleEasyData): ResponseEntity<SimpleEasyData> {
+        return ResponseEntity.ok(x)
+
+    }
 }
+
+data class SimpleEasyData(
+    @JsonProperty("requestTs") var date: LocalDateTime = LocalDateTime.now(),
+    @JsonProperty("name") var complexName: String? = "test"
+)
